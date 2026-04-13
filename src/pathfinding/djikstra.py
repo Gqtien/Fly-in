25fb024ef0f60dc2dfcc860e66194512e9463759
Gqtien <bin.gatien@gmail.com>
2026-04-13 16:26:32 +0200

    wip: djikstra pathfinding

--- src/pathfinding/djikstra.py
+++ src/pathfinding/djikstra.py
@@ -1,3 +1,35 @@
+from models import MapData, Hub
+from graph import Graph, GraphType
+from typing import TypeAlias
+
+PathType: TypeAlias = tuple[float, list[str]]
+
+
 class Djikstra:
-    def __init__(self) -> None:
-        ...
+    def __init__(self, data: MapData) -> None:
+        self.data: MapData = data
+        self.graph: GraphType = Graph().map_to_graph(self.data)
+
+    def find_path(self) -> PathType:
+        stack: list[Hub] = []
+        visited: list[Hub] = []
+        start: Hub = self.data.hubs[self.data.start_hub]
+        stack.append(start)
+        visited.append(start)
+
+        while stack:
+            current: Hub = stack[-1]
+            neighbors: list[str] = self.graph.get(current.name, [])
+            unvisited: list[Hub] = [
+                self.data.hubs[n] for n in neighbors if n not in visited
+            ]
+
+        if unvisited:
+            neighbor: Hub = unvisited[0]
+            # forward drone
+            visited.append(neighbor)
+            stack.append(neighbor)
+        else:
+            stack.pop()
+
+        return 0.0, []
