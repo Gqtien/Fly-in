bfd4316e5b6540543ccb4b21fc20bdaf38215a1d
Gqtien <bin.gatien@gmail.com>
2026-04-16 16:32:05 +0200

    chore: road tile under every hub

--- src/visualization/computer.py
+++ src/visualization/computer.py
@@ -7,10 +7,10 @@ class Computer:
 
     def compute_roads_pos(self) -> dict[RoadType, Road]:
         roads: dict[RoadType, Road] = {}
-        default: RoadType = ((0, 0, 0), (0, 0, 0), (1, 1, 1))
+        default: RoadType = ((0, 0, -1), (0, 0, 0))
         roads[default] = Road.END
         for _, hub in self.data.hubs.items():
-            ...
+            roads[((hub.y * 20, 0, -(hub.x * 20)), (0, 0, 0))] = Road.STRAIGHT
         return roads
 
     def compute_camera_pos(self) -> tuple[float, float, float]:
