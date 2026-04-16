1539294474f8fd64a88628fd48707dfb5315355d
Gqtien <bin.gatien@gmail.com>
2026-04-16 09:59:40 +0200

    feat(visualization): keyboard controls

---
+++ src/visualization/controller.py
@@ -0,0 +1,21 @@
+import ursina as ur
+from models import MapData
+from .computer import Computer
+
+
+class Controller(ur.Entity):
+    def __init__(self, data: MapData) -> None:
+        super().__init__()
+        self.data = data
+        self.computer = Computer(self.data)
+        self.cam_pos = self.computer.compute_camera_pos()
+
+    def input(self, key: str) -> None:
+        match key:
+            case "escape":
+                ur.application.quit()
+            case "r":
+                self.recenter()
+
+    def recenter(self) -> None:
+        ur.camera.position = (*self.cam_pos,)
